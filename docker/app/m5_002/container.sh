59ed2bad95f9e4f47b54bdf7557f0b1c778c7fa5
Justin Littman <justinlittman@gwu.edu>
2015-02-04 07:43:08 -0500

    Updated README. Added docker-compose.yml example files.  Added/renamed app configurations. Deleted container.sh scripts.

--- docker/app/m5_002/container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -d --name "sfm_app" \
-    --env-file ./env.list \
-    --link sfm_db:db \
-    -p 8000:80 \
-    gwul/sfm_app
--- docker/app/m5_002/example.container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -d --name "sfm_dev" \
-    --env-file ./env.list \
-    --link sfm_db:db \
-    -p 8000:80 \
-    -v ~/Data/sfm/social-feed-manager:/opt/social-feed-manager \
-    gwul/sfm_dev
