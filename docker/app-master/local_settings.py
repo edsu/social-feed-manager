054f27c1241eaf30a9eabf4b8feb0c6174d40d5c
Justin Littman <justinlittman@gwu.edu>
2015-02-06 08:20:56 -0500

    Added branding configuration.

--- docker/app-master/local_settings.py
+++ docker/app-master/local_settings.py
@@ -59,11 +59,12 @@ BRANDING = {
     # Required:
     'institution': os.getenv('SFM_BRANDING_INSTITUTION', ''),
     'URL': os.getenv('SFM_BRANDING_URL', ''),
-    # Optional. Address may contain any number of elements.
-    # However, for configuration by env variables, only allowing one.
+    # Optional:
+    #   address may contain any number of elements
     'address': [os.getenv('SFM_BRANDING_ADDRESS', '')],
     'email': os.getenv('SFM_BRANDING_EMAIL', ''),
     #   logofile should be placed in static/img
+    # See https://github.com/gwu-libraries/social-feed-manager/issues/300
     'logofile': '',
 }
 
